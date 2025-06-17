e59df562b043278a5a012a01923fad01f5a85426
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-06-17 22:23:10 +0300

    wrapped in private / public routes

--- src/3-widgets/footer/ui/Footer.tsx
+++ src/3-widgets/footer/ui/Footer.tsx
@@ -1,17 +1,22 @@
 import { ConverterIcon, RatesIcon } from '@assets/icons';
 
+import { useAppSelector } from '@process/hooks';
 import { NavItem } from '@features/navigation/ui';
+import { AuthRequired } from '@shared/ui/AuthRequired';
 
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
+  const isUserAuth = useAppSelector((state) => state.user.isAuth);
   return (
-    <footer className={styles.footer}>
-      <div className={styles.mobileNav}>
-        <NavItem to="/rates" icon={<RatesIcon />} label="Rates" />
-        <NavItem to="/convert" icon={<ConverterIcon />} label="Convert" />
-      </div>
-      <div className={styles.desktopInfo}>Rates App, 2025</div>
-    </footer>
+    <AuthRequired isAuth={isUserAuth}>
+      <footer className={styles.footer}>
+        <div className={styles.mobileNav}>
+          <NavItem to="/rates" icon={<RatesIcon />} label="Rates" />
+          <NavItem to="/convert" icon={<ConverterIcon />} label="Convert" />
+        </div>
+        <div className={styles.desktopInfo}>Rates App, 2025</div>
+      </footer>
+    </AuthRequired>
   );
 };
