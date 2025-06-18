8d35cab207051df93e82777e8991801359130ca6
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-06-18 18:36:41 +0300

    fixes project colors & fixed Button props

--- src/6-shared/ui/Button/Button.tsx
+++ src/6-shared/ui/Button/Button.tsx
@@ -6,17 +6,10 @@ import styles from './Button.module.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'primary' | 'secondary' | 'default';
-  size?: 'sm' | 'md' | 'lg';
 };
 
-export const Button = ({
-  children,
-  className,
-  variant = 'primary',
-  size = 'md',
-  ...props
-}: ButtonProps) => {
-  const classNames = joinClassNames(className, styles.button, styles[variant], styles[size]);
+export const Button = ({ children, className, variant = 'primary', ...props }: ButtonProps) => {
+  const classNames = joinClassNames(className, styles.button, styles[variant]);
   return (
     <button {...props} className={classNames}>
       {children}
