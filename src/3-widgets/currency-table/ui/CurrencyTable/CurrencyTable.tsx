4e27fdd37382341d37cacfdf255d97046526759f
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-06-21 21:05:45 +0300

    add sort for currency table &  div to table tag structure

--- src/3-widgets/currency-table/ui/CurrencyTable/CurrencyTable.tsx
+++ src/3-widgets/currency-table/ui/CurrencyTable/CurrencyTable.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Big from 'big.js';
 
 import { useAppDispatch, useAppSelector } from '@process/hooks';
@@ -22,14 +22,20 @@ export const CurrencyTable = () => {
   const error = useAppSelector(selectError);
   const page = useAppSelector(selectCurrencyPage);
   const pageSize = useAppSelector(selectCurrencyPageSize);
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
 
   const visibleData = useMemo(() => {
     const start = (page - 1) * pageSize;
     const end = start + pageSize;
     return ratesCodes
       .slice(start, end)
-      .map((code) => ({ code, value: Big(rates[code]).round(18) }));
-  }, [page, pageSize, ratesCodes, rates]);
+      .sort((a, b) => {
+        const rateA = Big(rates[a]);
+        const rateB = Big(rates[b]);
+        return sortDirection === 'asc' ? rateA.cmp(rateB) : rateB.cmp(rateA);
+      })
+      .map((name) => ({ name, value: Big(rates[name]).round(18) }));
+  }, [page, pageSize, sortDirection, ratesCodes, rates]);
 
   useEffect(() => {
     dispatch(currencyModel.loadRates());
@@ -39,18 +45,31 @@ export const CurrencyTable = () => {
     const intervalId = setInterval(() => {
       dispatch(currencyModel.loadRates());
     }, 30000);
-
     return () => clearInterval(intervalId);
   }, [dispatch]);
+
+  const handleSort = () => {
+    setSortDirection((prev) => (prev === 'asc' ? 'desc' : 'asc'));
+  };
 
   if (isLoading) return <Loader />;
   if (error) return <div className={styles.error}>{error}</div>;
 
   return (
-    <div className={styles.wrapper}>
-      {visibleData.map(({ code, value }) => (
-        <CurrencyRow code={code} key={code} value={value} />
-      ))}
-    </div>
+    <table className={styles.curencyTable}>
+      <thead className={styles.thead}>
+        <tr className={styles.thead__row}>
+          <th className={styles.thead__row_name}>Name</th>
+          <th className={styles.thead__row_rate} onClick={handleSort}>
+            Rate
+          </th>
+        </tr>
+      </thead>
+      <tbody className={styles.tbody}>
+        {visibleData.map(({ name, value }) => (
+          <CurrencyRow name={name} key={name} value={value} />
+        ))}
+      </tbody>
+    </table>
   );
 };
