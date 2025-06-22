851c947651320115e2b5aa996a7878d662c711f9
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-06-22 22:00:29 +0300

    add proxy-server for fetch to coingate-api

--- src/6-shared/api/rates.ts
+++ src/6-shared/api/rates.ts
@@ -1,7 +1,7 @@
 import type { StringRecord } from './types';
 
 export async function fetchRates(): Promise<StringRecord> {
-  const res = await fetch('https://api.coingate.com/v2/rates/merchant');
+  const res = await fetch(`/coingate-api/v2/rates/merchant`);
   if (!res.ok) {
     throw new Error(`Ошибка при загрузке курсов валют: ${res.statusText}`);
   }
@@ -11,5 +11,9 @@ export async function fetchRates(): Promise<StringRecord> {
 
 // export async function fetchRates(): Promise<StringRecord> {
 //   await new Promise((resolve) => setTimeout(resolve, 1500));
-//   return rates.USD;
+//   const test = Object.entries(rates.USD).reduce(
+//     (acc, [key, value]) => ({ ...acc, [key]: `${Number(value) * Math.random() * 100}` }),
+//     {},
+//   );
+//   return test;
 // }
